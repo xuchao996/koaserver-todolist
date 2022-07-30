034d8286d89052364a50ebf7f28246d779c01809
徐超 <chao.xu996@gmail.com>
2022-07-30 17:01:52 +0800

    feat: 接入loadEnv 切换环境变量.

---
+++ server/src/utils/loadEnv.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+console.log(process.cwd());
+
+/**
+ *
+ * @param {'' | 'local' | 'prod' | 'test'} mode
+ */
+const loadEnv = (mode = '') => {
+  const envFile = mode ? `.env.${mode}` : `.env`;
+  const rootDir = process.cwd();
+  const filePath = path.resolve(rootDir, envFile);
+  const localPath = path.resolve(rootDir, `.env.local`);
+  const load = (loadPath) => {
+    try {
+      const env = dotenv.config({
+        path: loadPath,
+        debug: process.env.debug,
+      });
+      dotenvExpand.expand(env);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+  load(filePath); // 取前面一个值
+  load(localPath);
+  console.log(process.env);
+};
+
+module.exports = loadEnv;
